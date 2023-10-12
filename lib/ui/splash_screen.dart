2905757a7e6d679ba02f9a68756c624608552ad7
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-10-12 20:56:42 +0700

    Add splash screen

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,42 @@
+import "dart:async";
+
+import "package:flutter/material.dart";
+import "package:niture_app/ui/home_screen.dart";
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Image.asset(
+            "assets/images/logo_app.jpg",
+            width: 230,
+          ),
+        ),
+      ),
+    );
+  }
+}
