fc8f649900ac1a10dab73c5febeb618941098732
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-10-13 23:38:36 +0700

    Add banner & update some code

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -18,9 +18,74 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               _buildHeader(),
+              _buildBanner(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container _buildBanner() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25.0,
+        vertical: 35.0,
+      ),
+      margin: const EdgeInsets.symmetric(
+        vertical: 20.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          15.0,
+        ),
+        image: const DecorationImage(
+          image: AssetImage(
+            "assets/images/img_banner.png",
+          ),
+          fit: BoxFit.cover,
+          alignment: Alignment.bottomCenter,
+        ),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Discover Furniture \nHigh Quality",
+                style: comfortaaTextStyle(
+                  color: const Color(0xff4E5471),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "70%",
+                    style: comfortaaTextStyle(
+                      color: const Color(0xff4E5471),
+                      fontWeight: comfortaaBold,
+                      fontSize: 35.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 7.0,
+                  ),
+                  Text(
+                    "off",
+                    style: comfortaaTextStyle(
+                      color: const Color(0xff4E5471),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -32,9 +97,9 @@ class _HomeScreenState extends State<HomeScreen> {
         Row(
           children: [
             InkWell(
-              radius: 25,
+              radius: 25.0,
               onTap: () {},
-              borderRadius: BorderRadius.circular(50),
+              borderRadius: BorderRadius.circular(50.0),
               child: const Padding(
                 padding: EdgeInsets.all(5.0),
                 child: CircleAvatar(
@@ -55,12 +120,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontWeight: comfortaaBold,
                   ),
                 ),
+                const SizedBox(
+                  height: 6.0,
+                ),
                 Text(
                   "Find the best home furniture",
                   style: comfortaaTextStyle(
                     fontWeight: comfortaaLight,
                     color: const Color(0xFF808991),
-                    fontSize: 14,
+                    fontSize: 14.0,
                   ),
                 )
               ],
@@ -71,9 +139,9 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () {},
           icon: Image.asset(
             "assets/images/ic_search.png",
-            width: 24,
+            width: 24.0,
           ),
-          splashRadius: 20,
+          splashRadius: 20.0,
         ),
       ],
     );
