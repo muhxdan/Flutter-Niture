1875fcb876ed821e0b67a6d60d4e4e74821779fa
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-10-15 01:02:45 +0700

    Feature/home screen
    * Add home screen
    
    * Add header
    
    * Add banner & update some code
    
    * Add temporary grid view
    
    * Update list & some code

---
+++ lib/model/furniture_data.dart
@@ -0,0 +1,74 @@
+class FurnitureData {
+  String name;
+  String price;
+  String image;
+  String description;
+  int totalReviews;
+  double rating;
+
+  FurnitureData({
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.description,
+    required this.totalReviews,
+    required this.rating,
+  });
+}
+
+var furnitureList = [
+  FurnitureData(
+    name: "Black Simple Lamp",
+    price: "12",
+    image: "assets/images/img_black_lamp.png",
+    description:
+        "Enhance the ambiance of your living space with this sleek and modern black lamp. Its minimalistic design and premium build quality make it a perfect addition to any room. The soft, warm glow it provides adds elegance and sophistication to your home decor. Whether you place it on your bedside table, your desk, or in your living room, this lamp will not only illuminate your space but also serve as a stylish decorative element.",
+    totalReviews: 76,
+    rating: 4.4,
+  ),
+  FurnitureData(
+    name: "Minimalist side table",
+    price: "25",
+    image: "assets/images/img_side_table.png",
+    description:
+        "This minimalist side table is a versatile and elegant piece of furniture that complements any room in your home. Its clean and simple design allows it to seamlessly blend with a variety of interior styles. Crafted with high-quality materials, this side table is both sturdy and stylish. Whether you use it to hold your books, display decorative items, or simply as a convenient surface for your coffee cup, it's a practical and beautiful addition to your living space.",
+    totalReviews: 85,
+    rating: 4.6,
+  ),
+  FurnitureData(
+    name: "Jan Sflanaganvik sofa",
+    price: "499",
+    image: "assets/images/img_jan_sofa.png",
+    description:
+        "Experience the ultimate comfort and style with the Jan Sflanaganvik sofa. This premium sofa is designed for relaxation and luxury. Its plush cushions and ergonomic design provide exceptional comfort, making it the perfect centerpiece for your living room. Crafted with high-quality materials and attention to detail, this sofa not only offers a comfortable seating experience but also adds a touch of sophistication to your home decor. Whether you're entertaining guests or lounging with your family, this sofa is the epitome of modern elegance and comfort.",
+    totalReviews: 127,
+    rating: 4.7,
+  ),
+  FurnitureData(
+    name: "Simple desk",
+    price: "50",
+    image: "assets/images/img_simple_desk.png",
+    description:
+        "This simple desk is a practical and versatile addition to your workspace. Its clean and functional design provides a dedicated place for you to focus and be productive. Crafted with durable materials, it can withstand the demands of your work or study routine. The ample workspace accommodates your computer, books, and other essentials, while the minimalist design fits seamlessly into any room. Whether you're working from home or need a dedicated study area, this desk offers both simplicity and functionality.",
+    totalReviews: 54,
+    rating: 4.5,
+  ),
+  FurnitureData(
+    name: "Norrviken chair and table",
+    price: "70",
+    image: "assets/images/img_simple_desk.png",
+    description:
+        "The Norrviken chair and table set is your go-to solution for outdoor relaxation. The comfortable chair and matching table are perfect for creating a cozy corner in your garden, patio, or balcony. Crafted from weather-resistant materials, they are designed to withstand the elements while providing you with a comfortable seating experience. Whether you want to enjoy a cup of coffee, read a book, or simply soak up the sun, this set offers both style and functionality for your outdoor space. The Norrviken chair and table set is your ticket to a tranquil outdoor retreat.",
+    totalReviews: 43,
+    rating: 4.6,
+  ),
+  FurnitureData(
+    name: "Eames chair",
+    price: "20",
+    image: "assets/images/img_eames.png",
+    description:
+        "The iconic Eames chair is a timeless classic known for its exceptional design and comfort. This chair is a symbol of mid-century modern style and has remained popular for decades. Its ergonomically designed seat and sturdy construction make it not only a beautiful piece of furniture but also a practical and comfortable seating option. Whether you use it as a dining chair, an accent piece in your living room, or a stylish addition to your office, the Eames chair adds a touch of sophistication and luxury to your space.",
+    totalReviews: 93,
+    rating: 4.7,
+  ),
+];
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,218 @@
+import "package:flutter/material.dart";
+import "package:niture_app/model/furniture_data.dart";
+import "package:niture_app/utils/themes/styles.dart";
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              _buildHeader(),
+              _buildListFurniture(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildListFurniture() {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildBanner(),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                double screenWidth = constraints.maxWidth;
+
+                int crossAxisCount;
+                if (screenWidth <= 600.0) {
+                  crossAxisCount = 2;
+                } else if (screenWidth <= 1200.0) {
+                  crossAxisCount = 4;
+                } else {
+                  crossAxisCount = 6;
+                }
+
+                return GridView.count(
+                  crossAxisCount: crossAxisCount,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: (3 / 4.7),
+                  shrinkWrap: true,
+                  physics: const ScrollPhysics(),
+                  children: furnitureList.map((furniture) {
+                    return InkWell(
+                      borderRadius: BorderRadius.circular(10.0),
+                      onTap: () {},
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.asset(
+                                  furniture.image,
+                                  fit: BoxFit.cover,
+                                  height: 160,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      furniture.name,
+                                      style: comfortaaTextStyle(
+                                        fontWeight: comfortaaLight,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    Text(
+                                      "\$${furniture.price}",
+                                      style: comfortaaTextStyle(
+                                        fontWeight: comfortaaBold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildBanner() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25.0,
+        vertical: 35.0,
+      ),
+      margin: const EdgeInsets.only(
+        bottom: 20.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          15.0,
+        ),
+        image: const DecorationImage(
+          image: AssetImage(
+            "assets/images/img_banner.png",
+          ),
+          fit: BoxFit.cover,
+          alignment: Alignment.bottomCenter,
+        ),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Discover Furniture \nHigh Quality",
+                style: comfortaaTextStyle(
+                  color: const Color(0xff4E5471),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "70%",
+                    style: comfortaaTextStyle(
+                      color: const Color(0xff4E5471),
+                      fontWeight: comfortaaBold,
+                      fontSize: 35.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 7.0,
+                  ),
+                  Text(
+                    "off",
+                    style: comfortaaTextStyle(
+                      color: const Color(0xff4E5471),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildHeader() {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hi, Welcome Back!👋",
+                style: comfortaaTextStyle(
+                  fontWeight: comfortaaBold,
+                ),
+              ),
+              const SizedBox(
+                height: 6.0,
+              ),
+              Text(
+                "Find the best home furniture",
+                style: comfortaaTextStyle(
+                  fontWeight: comfortaaLight,
+                  color: const Color(0xFF808991),
+                  fontSize: 14.0,
+                ),
+              )
+            ],
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              "assets/images/ic_search.png",
+              width: 24.0,
+            ),
+            splashRadius: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
